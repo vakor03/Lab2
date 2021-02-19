44391760c0a231f7efbbb2a38c28f8320c02e110
Valerii_kor <myksv200@gmail.com>
2021-02-19 23:47:48 +0300

    TODO list

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -6,6 +6,10 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
+            //TODO: Read and write to/from file
+            //TODO: Reading LineByLine
+            //TODO: Checking results
+            //TODO: Resulting table .csv
             Console.WriteLine("Hello World!");
         }
     }
