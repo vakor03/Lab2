be7ed426a617d6f2395b523f9b2438aa064695fc
Valerii_kor <myksv200@gmail.com>
2021-02-21 15:58:45 +0300

    Teams class
    New class + constructor from string

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -10,7 +10,11 @@ namespace Lab2
             //TODO: Reading LineByLine
             //TODO: Checking results
             //TODO: Resulting table .csv
-            Console.WriteLine("Hello World!");
+            Teams team1 = new Teams("Chelsea FC,3:0,0:0,1:0,0:2,4:1,3:1,0:3,1:5,0:2,4:1");
+            team1.ToString();
         }
+
+        
+        
     }
 }
---
+++ Lab2/Teams.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lab2
+{
+    public class Teams
+    {
+        private int Points;
+        private string Name;
+
+        public Teams(string str)
+        {
+            string[] scores = str.Split(',');
+            int points = 0;
+            Name = scores[0];
+            for (int i = 1; i < scores.Length; i++)
+            {
+                int point1 = Int32.Parse(scores[i].Split(':')[0]);
+                int point2 = Int32.Parse(scores[i].Split(':')[1]);
+                if (point1>point2)
+                {
+                    points += 3;
+                }else if (point1 == point2)
+                {
+                    points += 1;
+                }
+            }
+            Points = points;
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine($"Team name: {Name}, points: {Points}");
+        }
+    }
+}
